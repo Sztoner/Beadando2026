5ca6f9e1c92114e3f67789d62770698257606c07
Sztoner <terszabolcs@gmail.com>
2026-03-04 17:59:45 +0100

    SzakemberMain Redesign

--- Client/Kliens/Kliens/UserControls/Szakember/SzakemberMain.Designer.cs
+++ Client/Kliens/Kliens/UserControls/Szakember/SzakemberMain.Designer.cs
@@ -28,62 +28,167 @@
         /// </summary>
         private void InitializeComponent()
         {
-            loadButton = new Button();
-            newButton = new Button();
-            projectBox = new ComboBox();
+            mainPanel = new Panel();
+            projectGridView = new DataGridView();
+            addProjectButton = new Button();
+            projectInfoPanel = new Panel();
+            partPanel = new Panel();
+            label1 = new Label();
+            projectStatusLabel = new Label();
+            contractorLabel = new Label();
+            projectDescLabel = new Label();
+            projectIDLabel = new Label();
+            projectNameLabel = new Label();
+            mainPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)projectGridView).BeginInit();
+            projectInfoPanel.SuspendLayout();
             SuspendLayout();
             // 
-            // loadButton
+            // mainPanel
             // 
-            loadButton.BackColor = Color.DodgerBlue;
-            loadButton.FlatStyle = FlatStyle.Flat;
-            loadButton.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 238);
-            loadButton.Location = new Point(420, 186);
-            loadButton.Name = "loadButton";
-            loadButton.Size = new Size(100, 100);
-            loadButton.TabIndex = 1;
-            loadButton.Text = "Betöltés";
-            loadButton.UseVisualStyleBackColor = false;
+            mainPanel.Controls.Add(projectGridView);
+            mainPanel.Controls.Add(addProjectButton);
+            mainPanel.Controls.Add(projectInfoPanel);
+            mainPanel.Dock = DockStyle.Fill;
+            mainPanel.Location = new Point(0, 0);
+            mainPanel.Name = "mainPanel";
+            mainPanel.Size = new Size(800, 450);
+            mainPanel.TabIndex = 2;
             // 
-            // newButton
+            // projectGridView
             // 
-            newButton.BackColor = Color.DodgerBlue;
-            newButton.FlatStyle = FlatStyle.Flat;
-            newButton.Font = new Font("Segoe UI", 39.75F, FontStyle.Bold, GraphicsUnit.Point, 238);
-            newButton.Location = new Point(280, 186);
-            newButton.Name = "newButton";
-            newButton.Size = new Size(100, 100);
-            newButton.TabIndex = 0;
-            newButton.Text = "+";
-            newButton.UseVisualStyleBackColor = false;
+            projectGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            projectGridView.Location = new Point(24, 83);
+            projectGridView.Name = "projectGridView";
+            projectGridView.Size = new Size(360, 297);
+            projectGridView.TabIndex = 5;
             // 
-            // projectBox
+            // addProjectButton
             // 
-            projectBox.Font = new Font("Segoe UI", 11F);
-            projectBox.FormattingEnabled = true;
-            projectBox.Location = new Point(526, 186);
-            projectBox.Name = "projectBox";
-            projectBox.Size = new Size(163, 28);
-            projectBox.TabIndex = 2;
-            projectBox.Visible = false;
+            addProjectButton.BackColor = Color.DodgerBlue;
+            addProjectButton.FlatStyle = FlatStyle.Flat;
+            addProjectButton.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            addProjectButton.ForeColor = Color.Black;
+            addProjectButton.Location = new Point(24, 47);
+            addProjectButton.Name = "addProjectButton";
+            addProjectButton.Size = new Size(35, 30);
+            addProjectButton.TabIndex = 4;
+            addProjectButton.Text = "+";
+            addProjectButton.UseVisualStyleBackColor = false;
+            // 
+            // projectInfoPanel
+            // 
+            projectInfoPanel.AutoScroll = true;
+            projectInfoPanel.BackColor = Color.White;
+            projectInfoPanel.BorderStyle = BorderStyle.FixedSingle;
+            projectInfoPanel.Controls.Add(partPanel);
+            projectInfoPanel.Controls.Add(label1);
+            projectInfoPanel.Controls.Add(projectStatusLabel);
+            projectInfoPanel.Controls.Add(contractorLabel);
+            projectInfoPanel.Controls.Add(projectDescLabel);
+            projectInfoPanel.Controls.Add(projectIDLabel);
+            projectInfoPanel.Controls.Add(projectNameLabel);
+            projectInfoPanel.Dock = DockStyle.Right;
+            projectInfoPanel.Location = new Point(409, 0);
+            projectInfoPanel.Name = "projectInfoPanel";
+            projectInfoPanel.Size = new Size(391, 450);
+            projectInfoPanel.TabIndex = 3;
+            // 
+            // partPanel
+            // 
+            partPanel.Location = new Point(4, 394);
+            partPanel.Name = "partPanel";
+            partPanel.Size = new Size(350, 225);
+            partPanel.TabIndex = 8;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Calibri", 11F);
+            label1.Location = new Point(4, 361);
+            label1.Name = "label1";
+            label1.Size = new Size(80, 18);
+            label1.TabIndex = 7;
+            label1.Text = "Alkatrészek";
+            // 
+            // projectStatusLabel
+            // 
+            projectStatusLabel.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            projectStatusLabel.AutoSize = true;
+            projectStatusLabel.Font = new Font("Calibri", 11F);
+            projectStatusLabel.Location = new Point(317, 46);
+            projectStatusLabel.Name = "projectStatusLabel";
+            projectStatusLabel.Size = new Size(52, 18);
+            projectStatusLabel.TabIndex = 4;
+            projectStatusLabel.Text = "Státusz";
+            // 
+            // contractorLabel
+            // 
+            contractorLabel.AutoSize = true;
+            contractorLabel.Font = new Font("Calibri", 11F);
+            contractorLabel.Location = new Point(4, 78);
+            contractorLabel.Name = "contractorLabel";
+            contractorLabel.Size = new Size(84, 18);
+            contractorLabel.TabIndex = 3;
+            contractorLabel.Text = "Megrendelö";
+            // 
+            // projectDescLabel
+            // 
+            projectDescLabel.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            projectDescLabel.Font = new Font("Calibri", 11F);
+            projectDescLabel.Location = new Point(4, 114);
+            projectDescLabel.Name = "projectDescLabel";
+            projectDescLabel.Size = new Size(365, 616);
+            projectDescLabel.TabIndex = 2;
+            projectDescLabel.Text = "Leírás";
+            // 
+            // projectIDLabel
+            // 
+            projectIDLabel.AutoSize = true;
+            projectIDLabel.Font = new Font("Calibri", 11F);
+            projectIDLabel.Location = new Point(3, 46);
+            projectIDLabel.Name = "projectIDLabel";
+            projectIDLabel.Size = new Size(69, 18);
+            projectIDLabel.TabIndex = 1;
+            projectIDLabel.Text = "Projekt ID";
+            // 
+            // projectNameLabel
+            // 
+            projectNameLabel.AutoSize = true;
+            projectNameLabel.Font = new Font("Calibri", 14.25F, FontStyle.Bold);
+            projectNameLabel.Location = new Point(3, 23);
+            projectNameLabel.Name = "projectNameLabel";
+            projectNameLabel.Size = new Size(114, 23);
+            projectNameLabel.TabIndex = 0;
+            projectNameLabel.Text = "Projekt Neve";
             // 
             // SzakemberMain
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            Controls.Add(projectBox);
-            Controls.Add(loadButton);
-            Controls.Add(newButton);
+            Controls.Add(mainPanel);
             Name = "SzakemberMain";
             Size = new Size(800, 450);
+            mainPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)projectGridView).EndInit();
+            projectInfoPanel.ResumeLayout(false);
+            projectInfoPanel.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
 
-        private Button loadButton;
-        private Button newButton;
-        private ComboBox projectBox;
+        private Panel mainPanel;
+        private Button addProjectButton;
+        private Panel projectInfoPanel;
+        private Label contractorLabel;
+        private Label projectDescLabel;
+        private Label projectIDLabel;
+        private Label projectNameLabel;
+        private DataGridView projectGridView;
+        private Label projectStatusLabel;
+        private Panel partPanel;
+        private Label label1;
     }
 }
